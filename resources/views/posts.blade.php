e33321efe511d653158c3892e6ababdadab91c79
ahmad raihan <ahmadraihanbuatsekolah@gmail.com>
2023-10-29 12:46:55 +0800

    Commit Video Ke 4

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>WPU Blog | Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+    
+    @foreach ($posts as $post)
+        <article class="mb-5">
+          <h2>
+            <a href="/posts/{{ $post["slug"] }}">{{ $post["tittle"] }}</a></h2>
+          <h5>By : {{ $post["author"] }}</h5>
+          <p>{{ $post["body"] }}</p>
+      </article>
+    @endforeach
+
+@endsection
+
